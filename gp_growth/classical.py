bd1abf022848f4cbdbe755c935ec0a0e27aa323e
Peter <peter.tonner@duke.edu>
2016-05-02 16:19:37 -0400

    updates

--- gp_growth/classical.py
+++ gp_growth/classical.py
@@ -11,6 +11,9 @@ def richards(t,m,A,l,v):
     p = 1 + v*np.exp(1+v)*np.exp(1.*m/A*(1+v)*(1+1./v)*(l-t))
     return 1.*A * np.power(p,-1./v)
 
+def schnute(t,m,A,l,a=.1,b=.1):
+    return (1.*m*(1-b)/a) * np.power((1-(b*np.exp(a*l+1-b-a*t)))/(1-b),1./b)
+
 def meanSquareError(time,od,model=gompertz):
 	# Gompertz Testing
 	try:
@@ -24,28 +27,43 @@ def meanSquareError(time,od,model=gompertz):
 		predict = np.array([gompertz(t,*popt) for t in time])
 		mse = 1./n * sum(((od - predict)**2),)
 	except RuntimeError, e:
-		mse = 1./n * sum(((od - np.mean(od))**2),)
+		mse = np.nan# 1./n * sum(((od - np.mean(od))**2),)
 
 	return mse
 
-def optimize(time,od,model=gompertz):
+def optimize(time,od,model=gompertz,p0=None,cv=None):
 
 	time = time.astype(float)
 	od = od.astype(float)
 	n = time.shape[0]
 
+	if cv:
+		train_time = time[cv[0]]
+		train_od = od[cv[0]]
+
+		test_time = time[cv[1]]
+		test_od = od[cv[1]]
+
+	else:
+		train_time = time
+		train_od = od
+
+		test_time = time
+		test_od = od
+
+
 	try:
-		popt, pcov = curve_fit(model,time,od)
+		popt, pcov = curve_fit(model,train_time,train_od,p0=p0)
 
 		m,A,l = popt[:3]
 
 		if m <= 0:
 			raise RuntimeError()
 
-		predict = np.array([model(t,*popt) for t in time])
-		mse = 1./n * sum(((od - predict)**2),)
+		predict = np.array([model(t,*popt) for t in test_time])
+		mse = 1./test_od.shape[0] * sum(((test_od - predict)**2),)
 	except RuntimeError, e:
 		m,A,l = np.nan, np.nan, np.nan
-		mse = 1./n * sum(((od - np.mean(od))**2),)
+		mse = np.nan
 
 	return [m,A,l,mse]
--- setup_data.py
+++ setup_data.py
@@ -13,7 +13,7 @@ od = pd.DataFrame()
 for plate in plates:
 
 	print plate
-	
+
 	try:
 	    key,data = utils.load_bioscreen(os.path.join(hsal_dir,plate),removeBlank=False)
 	except ValueError, e:
@@ -35,8 +35,8 @@ od.columns=['time']+meta.index.tolist()
 
 combined = pd.merge(meta,od.T,left_index=True,right_index=True)
 combined.columns = combined.columns[:meta.shape[1]].tolist() + od.time.tolist()
+combined = combined[combined.Strain.isin(['ura3','rosR','trmB','asnC','idr1','idr2','sirR','VNG1179'])]
 
 meta.to_csv("data/processed/meta.csv",index=False)
 od.to_csv("data/processed/data.csv",index=False)
 combined.to_csv("data/processed/combined.csv",index=False)
-	
